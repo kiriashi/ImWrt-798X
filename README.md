e54829a8bc049b5e1297a211e3d8e41227b62352
kiriashi <2244193927@qq.com>
2025-08-29 09:41:19 +0800

    update

--- README.md
+++ README.md
@@ -4,14 +4,8 @@
 
 ## 功能
 - **支持的设备**：
-  - cmcc_a10
   - cmcc_rax3000m
   - cmcc_rax3000m-emmc
-  - cmcc_rax3000m-emmc-usboffload
-  - cmcc_rax3000m-usboffload
-  - cmcc_rax3000me
-  - cmcc_xr30
-  - cmcc_xr30-emmc
 - **自动编译**：每天（北京时间 08:00）检查源仓库更新，若有新提交，自动为所有 CMCC 机型编译固件。
 - **5G 25dB 增强**：支持启用 5G 高功率模式（默认启用，定时编译时固定启用，手动编译可选）。
 - **固件上传**：仅上传 `sysupgrade.bin` 文件到 GitHub Release 和 WebDAV，无 ZIP 压缩。
--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -9,14 +9,14 @@
 # 文件名: diy-part1.sh
 # 描述: OpenWrt DIY 脚本第一部分 (更新 feeds 之前)
 
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-git clone https://github.com/EasyTier/luci-app-easytier.git
-git clone https://github.com/chenmozhijin/luci-app-socat.git
-git clone https://github.com/asvow/luci-app-tailscale.git
-git clone https://github.com/sbwml/luci-app-mosdns.git
-git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
-git clone https://github.com/sbwml/luci-app-openlist2.git
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
+git clone https://github.com/chenmozhijin/luci-app-socat.git package/luci-app-socat
+git clone https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
+git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+git clone https://github.com/sirpdboy/luci-app-netspeedtest.git package/netspeedtest
+git clone https://github.com/sbwml/luci-app-openlist2.git package/luci-app-openlist2
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> "feeds.conf.default"
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
@@ -26,7 +26,6 @@ sed -i 's/msgstr \"Socat\"/msgstr \"端口转发\"/' package/luci-app-socat/luci
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-
-
+rm -rf  ./feeds/packages/net/speedtest-cli
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
